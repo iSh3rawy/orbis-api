9759f8bea59bb2f8b0aa32ad3a9f317417548902
Ahmed <ahmedelsharawy117@gmail.com>
2026-07-07 03:47:15 +0300

    refactor: call configure logging and use register app to error handlers

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-from core.logging import LoggingMiddleware
+from core.logging import configure_logging
+from core.middleware import LoggingMiddleware
+from core.error_handlers import register_exception_handlers
+
+configure_logging()
 
 app = FastAPI()
 
+
 app.add_middleware(LoggingMiddleware)
+register_exception_handlers(app)
 
 
 @app.get("/")
