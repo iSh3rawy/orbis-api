9a93f3b7102385cbfb049cdd7a8fe5f3f5ca2fa2
Ahmed <ahmedelsharawy117@gmail.com>
2026-07-07 07:50:08 +0300

    feat: auth router

---
+++ modules/auth/router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from modules.auth.schemas import UserCreateRequest, UserResponse
+from modules.users.service import create_user
+
+from core.database import get_db
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+@router.post("/register")
+def register_user(user_in: UserCreateRequest, db: Session = Depends(get_db)):
+    user = create_user(db, user_in)
+    return UserResponse.model_validate(user)
