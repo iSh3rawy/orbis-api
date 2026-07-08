40546e2058c203d33e07e451ff25212713e6f510
Ahmed <ahmedelsharawy117@gmail.com>
2026-07-09 00:29:19 +0300

    chore: add more exceptions

--- modules/auth/exceptions.py
+++ modules/auth/exceptions.py
@@ -4,3 +4,13 @@ from core.exceptions import UnauthorizedError
 class InvalidCredentialsError(UnauthorizedError):
     error_code = "INVALID_CREDENTIALS"
     message = "Invalid email or password"
+
+
+class InvalidTokenError(UnauthorizedError):
+    error_code = "INVALID_TOKEN"
+    message = "The provided token is invalid or expired"
+
+
+class MissingRefreshTokenError(UnauthorizedError):
+    error_code = "MISSING_REFRESH_TOKEN"
+    message = "Refresh token is missing from the request"
