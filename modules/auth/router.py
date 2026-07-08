d9903ccb0dcf8a524359eb976a187ad886db34e5
Ahmed <ahmedelsharawy117@gmail.com>
2026-07-08 09:02:27 +0300

    refactor(auth): use right status code and response schema

--- modules/auth/router.py
+++ modules/auth/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from modules.auth.schemas import RegisterRequest
 from modules.users.schemas import UserResponse
@@ -9,7 +9,9 @@ from core.database import get_db
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post("/register")
+@router.post(
+    "/register", status_code=status.HTTP_201_CREATED, response_model=UserResponse
+)
 def register_user(user_in: RegisterRequest, db: Session = Depends(get_db)):
     user = create_user(db, user_in)
     return UserResponse.model_validate(user)
