9c3719f27567f72a9b1f91245498b2b707a785e3
Ahmed <ahmedelsharawy117@gmail.com>
2026-07-08 09:37:16 +0300

    chore: add invalid credentials error

--- modules/users/exceptions.py
+++ modules/users/exceptions.py
@@ -4,3 +4,8 @@ from core.exceptions import ConflictError
 class EmailAlreadyExistsError(ConflictError):
     error_code = "EMAIL_ALREADY_EXISTS"
     message = "An account with this email already exists"
+
+
+class InvalidCredentialsError(ConflictError):
+    error_code = "INVALID_CREDENTIALS"
+    message = "Invalid email or password"
