acf183ee67e34bc5b973ba3f058a9935eaf6a328
Ahmed <ahmedelsharawy117@gmail.com>
2026-07-09 00:05:10 +0300

    refactor: use api version

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from core.logging import configure_logging
 from core.middleware import LoggingMiddleware
 from core.error_handlers import register_exception_handlers
+from core.config import settings
 
 from modules.auth.router import router as auth_router
 
@@ -9,7 +10,7 @@ configure_logging()
 
 app = FastAPI()
 
-API_PREFIX = "/api/v1"
+API_PREFIX = f"/api/v{settings.API_VERSION}"
 
 app.include_router(auth_router, prefix=API_PREFIX)
 
